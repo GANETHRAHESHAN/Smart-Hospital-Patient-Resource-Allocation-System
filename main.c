6c68cba5093a6f3b65dc17ba12d1f7ad8d2e1197
GANETHRAHESHAN <heshanpathirana1234@gmail.com>
2026-09-16 22:51:27 +0530

    add main menu

--- main.c
+++ main.c
@@ -13,8 +13,26 @@ const char wardName[4][30] = {"Genaral Ward", "Paediatric Ward", "Surgical Ward"
 const float daliyBedRate[4] = {3000.00, 6000.00, 12000.00, 25000.00};
 const int bedCapacity[4] = {20, 10, 10, 5};
 
+int choice;
+
 int main()
 {
-    printf("Hello world!\n");
+
+    do {
+    printf("=============================================================\n");
+    printf("     SMART HOSPITAL PATIENT & RESOURCE ALLOCATION SYSTEM\n");
+    printf("=============================================================\n");
+    printf("1. Register New Patient\n");
+    printf("2. View Bed Status\n");
+    printf("3. Display Patients by Priority Oder\n");
+    printf("4. Generate Performance Report\n");
+    printf("5. Exit\n");
+    printf("-------------------------------------------------------------\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+
+
+    } while (choice != 5);
     return 0;
 }
